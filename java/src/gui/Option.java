940a35053219947413aadb0d0dd870d09c207ae6
brianso217 <brianso217@gmail.com>
2020-10-16 16:22:43 -0400

    Added different variables into Client Frame and Owner Frame

--- java/src/gui/Option.java
+++ java/src/gui/Option.java
@@ -4,24 +4,25 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.UUID;
 
 public class Option extends JFrame implements ActionListener {
 
 	JPanel firstPanel, secondPanel, thirdPanel;
 	private JButton client, owner, submit1, submit2, help, back;
 	// Owners button
-	private JLabel ownerID, vehInfoTitle, approxRes, approxDaysTitle, approxMonsTitle, TEST2;
+	private JLabel ownerID, vehModel, vehColor, vehLicense, approxRes, approxDaysTitle, approxMonsTitle, TEST2;
 	// Clients button
-	private JLabel clientID, approxJobTime, approxJobHoursLabel, approxJobMinLabel, deadline, deadlineHoursLabel,
+	private JLabel clientID, jobInfo, approxJobTime, approxJobHoursLabel, approxJobMinLabel, deadline, deadlineHoursLabel,
 			deadlineMinLabel;
-	private JTextField ownerIDText, clientIDText;
-	private JTextArea vehInfo;
-	private JComboBox approxDays, approxMons, approxJobHours, approxJobMin, deadlineHours, deadlineMin;
+	private JTextField ownerIDText, vehModelText, vehLicenseText, clientIDText;
+	private JTextArea jobInfoText;
+	private JComboBox vehColorBox, approxDays, approxMons, approxJobHours, approxJobMin, deadlineHours, deadlineMin;
 	private int xPosAlignLeft = 100;
 	private int xPosAlignRight = 270;
 	CardLayout swap;
 	private Container content;
-
+	private String[] colors= {"White", "Black", "Grey", "Silver", "Green", "Red", "Blue", "Yellow", "Purple", "Pink", "Orange", "Other"};
 	// Opening frame to the GUI
 	public void panel1() {
 		firstPanel = new JPanel();
@@ -44,71 +45,85 @@ public class Option extends JFrame implements ActionListener {
 	public void clientPanel() {
 		secondPanel = new JPanel();
 		secondPanel.setLayout(null);
-		clientID = new JLabel("Client ID");
-		clientID.setFont(new Font("Arial", Font.PLAIN, 15));
-		clientID.setSize(150, 20);
-		clientID.setLocation(xPosAlignLeft, 50);
-		secondPanel.add(clientID);
+        clientID = new JLabel("Client ID");
+        clientID.setFont(new Font("Arial", Font.PLAIN, 15));
+        clientID.setSize(150, 20);
+        clientID.setLocation(xPosAlignLeft, 50);
+        secondPanel.add(clientID);
+        
+        clientIDText = new JTextField();
+        clientIDText.setFont(new Font("Arial", Font.PLAIN, 20));
+        clientIDText.setSize(90, 20);
+        clientIDText.setLocation(xPosAlignRight, 50);
+        secondPanel.add(clientIDText);
+        
+        jobInfo = new JLabel("Job Info");
+        jobInfo.setFont(new Font("Arial", Font.PLAIN, 15));
+        jobInfo.setSize(150, 20);
+        jobInfo.setLocation(xPosAlignLeft, 85);
+        secondPanel.add(jobInfo);
+        
+        jobInfoText = new JTextArea();
+        jobInfoText.setFont(new Font("Arial", Font.PLAIN, 20));
+        jobInfoText.setSize(300, 90);
+        jobInfoText.setLocation(xPosAlignRight, 85);
+        secondPanel.add(jobInfoText);
+        
+        approxJobTime = new JLabel("Approx. Job Time");
+        approxJobTime.setFont(new Font("Arial", Font.PLAIN, 15));
+        approxJobTime.setSize(125, 20);
+        approxJobTime.setLocation(xPosAlignLeft, 200);
+        secondPanel.add(approxJobTime);
+        
+        approxJobHoursLabel = new JLabel("Hours");
+        approxJobHoursLabel.setFont(new Font("Arial", Font.PLAIN, 10));
+        approxJobHoursLabel.setSize(60, 20);
+        approxJobHoursLabel.setLocation(xPosAlignRight, 180);
+        secondPanel.add(approxJobHoursLabel);
+        
+        approxJobMinLabel = new JLabel("Minutes");
+        approxJobMinLabel.setFont(new Font("Arial", Font.PLAIN, 10));
+        approxJobMinLabel.setSize(60, 20);
+        approxJobMinLabel.setLocation(xPosAlignRight+65, 180);
+        secondPanel.add(approxJobMinLabel);
+        
+        approxJobHours = new JComboBox();
+        approxJobHours.setFont(new Font("Arial", Font.PLAIN, 20));
+        approxJobHours.setSize(60, 25);
+        approxJobHours.setLocation(xPosAlignRight, 200);
+        approxJobMin = new JComboBox();
+        approxJobMin.setFont(new Font("Arial", Font.PLAIN, 20));
+        approxJobMin.setSize(60, 25);
+        approxJobMin.setLocation(xPosAlignRight+65, 200);
+        
+        
+        deadline = new JLabel("Deadline");
+        deadline.setFont(new Font("Arial", Font.PLAIN, 15));
+        deadline.setSize(125, 20);
+        deadline.setLocation(xPosAlignLeft, 275);
+        secondPanel.add(deadline);
+        
+        deadlineHoursLabel = new JLabel("Hours");
+        deadlineHoursLabel.setFont(new Font("Arial", Font.PLAIN, 10));
+        deadlineHoursLabel.setSize(60, 20);
+        deadlineHoursLabel.setLocation(xPosAlignRight, 255);
+        secondPanel.add(deadlineHoursLabel);
+        
+        deadlineMinLabel = new JLabel("Minutes");
+        deadlineMinLabel.setFont(new Font("Arial", Font.PLAIN, 10));
+        deadlineMinLabel.setSize(60, 20);
+        deadlineMinLabel.setLocation(xPosAlignRight+65, 255);
+        secondPanel.add(deadlineMinLabel);
+        
+        deadlineHours = new JComboBox();
+        deadlineHours.setFont(new Font("Arial", Font.PLAIN, 20));
+        deadlineHours.setSize(60, 25);
+        deadlineHours.setLocation(xPosAlignRight, 275);
+        deadlineMin = new JComboBox();
+        deadlineMin.setFont(new Font("Arial", Font.PLAIN, 20));
+        deadlineMin.setSize(60, 25);
+        deadlineMin.setLocation(xPosAlignRight+65, 275);
 
-		clientIDText = new JTextField();
-		clientIDText.setFont(new Font("Arial", Font.PLAIN, 20));
-		clientIDText.setSize(90, 20);
-		clientIDText.setLocation(xPosAlignRight, 50);
-		secondPanel.add(clientIDText);
-
-		approxJobTime = new JLabel("Approx. Job Time");
-		approxJobTime.setFont(new Font("Arial", Font.PLAIN, 15));
-		approxJobTime.setSize(125, 20);
-		approxJobTime.setLocation(xPosAlignLeft, 115);
-		secondPanel.add(approxJobTime);
-
-		approxJobHoursLabel = new JLabel("Hours");
-		approxJobHoursLabel.setFont(new Font("Arial", Font.PLAIN, 10));
-		approxJobHoursLabel.setSize(60, 20);
-		approxJobHoursLabel.setLocation(xPosAlignRight, 115);
-		secondPanel.add(approxJobHoursLabel);
-
-		approxJobMinLabel = new JLabel("Minutes");
-		approxJobMinLabel.setFont(new Font("Arial", Font.PLAIN, 10));
-		approxJobMinLabel.setSize(60, 20);
-		approxJobMinLabel.setLocation(xPosAlignRight + 65, 115);
-		secondPanel.add(approxJobMinLabel);
-
-		approxJobHours = new JComboBox();
-		approxJobHours.setFont(new Font("Arial", Font.PLAIN, 20));
-		approxJobHours.setSize(60, 25);
-		approxJobHours.setLocation(xPosAlignRight, 130);
-		approxJobMin = new JComboBox();
-		approxJobMin.setFont(new Font("Arial", Font.PLAIN, 20));
-		approxJobMin.setSize(60, 25);
-		approxJobMin.setLocation(xPosAlignRight + 65, 130);
-
-		deadline = new JLabel("Deadline");
-		deadline.setFont(new Font("Arial", Font.PLAIN, 15));
-		deadline.setSize(125, 20);
-		deadline.setLocation(xPosAlignLeft, 175);
-		secondPanel.add(deadline);
-
-		deadlineHoursLabel = new JLabel("Hours");
-		deadlineHoursLabel.setFont(new Font("Arial", Font.PLAIN, 10));
-		deadlineHoursLabel.setSize(60, 20);
-		deadlineHoursLabel.setLocation(xPosAlignRight, 150);
-		secondPanel.add(deadlineHoursLabel);
-
-		deadlineMinLabel = new JLabel("Minutes");
-		deadlineMinLabel.setFont(new Font("Arial", Font.PLAIN, 10));
-		deadlineMinLabel.setSize(60, 20);
-		deadlineMinLabel.setLocation(xPosAlignRight + 65, 150);
-		secondPanel.add(deadlineMinLabel);
-
-		deadlineHours = new JComboBox();
-		deadlineHours.setFont(new Font("Arial", Font.PLAIN, 20));
-		deadlineHours.setSize(60, 25);
-		deadlineHours.setLocation(xPosAlignRight, 175);
-		deadlineMin = new JComboBox();
-		deadlineMin.setFont(new Font("Arial", Font.PLAIN, 20));
-		deadlineMin.setSize(60, 25);
-		deadlineMin.setLocation(xPosAlignRight + 65, 175);
 
 		generateComboBoxVals(approxJobHours, deadlineHours, approxJobMin, deadlineMin);
 		secondPanel.add(approxJobHours);
@@ -123,6 +138,9 @@ public class Option extends JFrame implements ActionListener {
 				System.out.println("Success!");
 				System.out.println("Client ID:");
 				System.out.println(clientIDText.getText() + '\n');
+				UUID jobID= UUID.randomUUID();
+				System.out.println("Job ID: " + jobID + '\n');
+				System.out.println("Job Information: " + jobInfoText.getText() + '\n');
 				System.out.println("Expected job time: " + approxJobHours.getSelectedItem() + " hours | "
 						+ approxJobMin.getSelectedItem() + " minutes.");
 				System.out.println("Deadline: " + deadlineHours.getSelectedItem() + " hours | "
@@ -147,72 +165,101 @@ public class Option extends JFrame implements ActionListener {
 	public void ownerPanel() {
 		thirdPanel = new JPanel();
 		thirdPanel.setLayout(null);
-		ownerID = new JLabel("Owner ID");
-		ownerID.setFont(new Font("Arial", Font.PLAIN, 15));
-		ownerID.setSize(150, 20);
-		ownerID.setLocation(xPosAlignLeft, 50);
-		thirdPanel.add(ownerID);
+        ownerID = new JLabel("Owner ID");
+        ownerID.setFont(new Font("Arial", Font.BOLD, 15));
+        ownerID.setSize(150, 20);
+        ownerID.setLocation(xPosAlignLeft, 50);
+        thirdPanel.add(ownerID);
+        
+        ownerIDText = new JTextField();
+        ownerIDText.setFont(new Font("Arial", Font.PLAIN, 18));
+        ownerIDText.setSize(90, 20);
+        ownerIDText.setLocation(xPosAlignRight, 50);
+        thirdPanel.add(ownerIDText);
+        
+        vehModel = new JLabel("Vehicle Model");
+        vehModel.setFont(new Font("Arial", Font.BOLD, 15));
+        vehModel.setSize(150, 20);
+        vehModel.setLocation(xPosAlignLeft, 130);
+        thirdPanel.add(vehModel);
+        
+        vehModelText = new JTextField();
+        vehModelText.setFont(new Font("Arial", Font.PLAIN, 18));
+        vehModelText.setSize(175, 20);
+        vehModelText.setLocation(xPosAlignRight, 130);
+        thirdPanel.add(vehModelText);
+        
+        vehColor = new JLabel("Vehicle Color");
+        vehColor.setFont(new Font("Arial", Font.BOLD, 15));
+        vehColor.setSize(150, 20);
+        vehColor.setLocation(xPosAlignLeft, 185);
+        thirdPanel.add(vehColor);
+        
+        vehColorBox = new JComboBox();
+        vehColorBox.setFont(new Font("Arial", Font.PLAIN, 18));
+        vehColorBox.setSize(100, 25);
+        vehColorBox.setLocation(xPosAlignRight, 185);
+        insertColors(vehColorBox);
+        thirdPanel.add(vehColorBox);
+        
+        vehLicense = new JLabel("Vehicle Plate #");
+        vehLicense.setFont(new Font("Arial", Font.BOLD, 15));
+        vehLicense.setSize(150, 20);
+        vehLicense.setLocation(xPosAlignLeft, 255);
+        thirdPanel.add(vehLicense);
+        
+        vehLicenseText = new JTextField();
+        vehLicenseText.setFont(new Font("Arial", Font.PLAIN, 18));
+        vehLicenseText.setSize(100, 20);
+        vehLicenseText.setLocation(xPosAlignRight, 255);
+        thirdPanel.add(vehLicenseText);
+        
+        approxRes = new JLabel("Approx. Residency");
+        approxRes.setFont(new Font("Arial", Font.BOLD, 15));
+        approxRes.setSize(150, 20);
+        approxRes.setLocation(xPosAlignLeft, 315);
+        thirdPanel.add(approxRes);
+        
+        approxDaysTitle = new JLabel("Days");
+        approxDaysTitle.setFont(new Font("Arial", Font.PLAIN, 10));
+        approxDaysTitle.setSize(60, 20);
+        approxDaysTitle.setLocation(xPosAlignRight, 295);
+        thirdPanel.add(approxDaysTitle);
+        
+        approxMonsTitle = new JLabel("Months");
+        approxMonsTitle.setFont(new Font("Arial", Font.PLAIN, 10));
+        approxMonsTitle.setSize(60, 20);
+        approxMonsTitle.setLocation(xPosAlignRight+65, 295);
+        thirdPanel.add(approxMonsTitle);
+        
+        approxDays = new JComboBox();
+        approxDays.setFont(new Font("Arial", Font.PLAIN, 18));
+        approxDays.setSize(60, 25);
+        approxDays.setLocation(xPosAlignRight, 315);
+        approxMons = new JComboBox();
+        approxMons.setFont(new Font("Arial", Font.PLAIN, 18));
+        approxMons.setSize(60, 25);
+        approxMons.setLocation(xPosAlignRight+65, 315);
+        generateComboBoxVals(approxDays, approxMons);
+        thirdPanel.add(approxDays);
+        thirdPanel.add(approxMons);
 
-		ownerIDText = new JTextField();
-		ownerIDText.setFont(new Font("Arial", Font.PLAIN, 20));
-		ownerIDText.setSize(90, 20);
-		ownerIDText.setLocation(xPosAlignRight, 50);
-		thirdPanel.add(ownerIDText);
-
-		vehInfoTitle = new JLabel("Vehicle Information");
-		vehInfoTitle.setFont(new Font("Arial", Font.PLAIN, 15));
-		vehInfoTitle.setSize(150, 20);
-		vehInfoTitle.setLocation(xPosAlignLeft, 130);
-		thirdPanel.add(vehInfoTitle);
-
-		vehInfo = new JTextArea();
-		vehInfo.setFont(new Font("Arial", Font.PLAIN, 12));
-		vehInfo.setSize(300, 75);
-		vehInfo.setLocation(xPosAlignRight, 130);
-		thirdPanel.add(vehInfo);
-
-		approxRes = new JLabel("Approx. Residency");
-		approxRes.setFont(new Font("Arial", Font.PLAIN, 15));
-		approxRes.setSize(125, 20);
-		approxRes.setLocation(xPosAlignLeft, 280);
-		thirdPanel.add(approxRes);
-
-		approxDaysTitle = new JLabel("Days");
-		approxDaysTitle.setFont(new Font("Arial", Font.PLAIN, 10));
-		approxDaysTitle.setSize(60, 20);
-		approxDaysTitle.setLocation(xPosAlignRight, 255);
-		thirdPanel.add(approxDaysTitle);
-
-		approxMonsTitle = new JLabel("Months");
-		approxMonsTitle.setFont(new Font("Arial", Font.PLAIN, 10));
-		approxMonsTitle.setSize(60, 20);
-		approxMonsTitle.setLocation(xPosAlignRight + 65, 255);
-		thirdPanel.add(approxMonsTitle);
-
-		approxDays = new JComboBox();
-		approxDays.setFont(new Font("Arial", Font.PLAIN, 20));
-		approxDays.setSize(60, 25);
-		approxDays.setLocation(xPosAlignRight, 280);
-		thirdPanel.add(approxDays);
-		approxMons = new JComboBox();
-		approxMons.setFont(new Font("Arial", Font.PLAIN, 20));
-		approxMons.setSize(60, 25);
-		approxMons.setLocation(xPosAlignRight + 65, 280);
-		thirdPanel.add(approxMons);
-		generateComboBoxVals(approxDays, approxMons);
 
 		submit2 = new JButton("Submit");
 
 		submit2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent event) {
-
+				
 				System.out.println("Success!");
-				System.out.println("Owner ID:");
+				System.out.println("Owner ID: ");
 				System.out.println(ownerIDText.getText() + '\n');
-				System.out.println("Vehicle Info:");
-				System.out.println(vehInfo.getText() + '\n');
-				System.out.println("Expected days: " + approxDays.getSelectedItem() + " | Expected Months: "
-						+ approxMons.getSelectedItem());
+				System.out.println("Vehicle Model: ");
+				System.out.println(vehModelText.getText() + '\n');
+				System.out.println("Vehicle Color: " + vehColorBox.getSelectedItem() + '\n');
+				System.out.println("Vehicle Plate #: "+ vehLicenseText.getText() + '\n');
+				System.out.println("Expected days: " + approxDays.getSelectedItem() 
+						+ " | Expected Months: " + approxMons.getSelectedItem());
+
 				
 				//resets information for next use
 				clearInfo();
@@ -321,9 +368,18 @@ public class Option extends JFrame implements ActionListener {
 		
 		//owner info clear
 		ownerIDText.setText(null);
-		vehInfo.setText(null);
+		vehModelText.setText(null);
+		vehLicenseText.setText(null);
+		vehColorBox.setSelectedIndex(0);
 		approxDays.setSelectedIndex(0);
 		approxMons.setSelectedIndex(0);
+	}
+
+	
+	private void insertColors(JComboBox vehColorBox) {
+		for(int i=0; i<colors.length; i++) {
+			vehColorBox.addItem(colors[i]);
+		}
 	}
 
 	private void generateComboBoxVals(JComboBox approxDays, JComboBox approxMons) {
